7c76fc457fa90c25114335031db82dbcfa10b996
S-Vilka <sergei.vilka@metropolia.com>
2024-09-30 10:49:39 +0300

    Iteration 2 - done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
 import JobListings from "../components/JobListings";
 
 const Home = () => {
+
+  const [jobs, setJobs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch("/api/jobs");
+        if (!res.ok) {
+          throw new Error("Failed to fetch jobs");
+        }
+        const data = await res.json();
+        setJobs(data);
+        setIsPending(false);
+        setError(null);
+      } catch (error) {
+        setIsPending(false);
+        setError(error.message);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+
   return (
     <div className="home">
-      <JobListings  />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {jobs &&  <JobListings jobs={jobs} />}
     </div>
   );
 };
